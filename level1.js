7c5223f5eec6a2113daebb0bfbe9c6d1b9dc9c18
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-04-08 23:18:19 +0100

    update

--- level1.js
+++ level1.js
@@ -215,12 +215,15 @@ const total = epicArr.reduce((prev, curr) => {
 
 console.log(total);
 
-
+import countries from "";
+console.log(countries);
 
 // Declare a function called categorizeCountries which returns an array of countries which have some common pattern(you find the countries array in this repository as countries.js(eg 'land', 'ia', 'island','stan')).
 
 
 // Create a function which return an array of objects, which is the letter and the number of times the letter use to start with a name of a country.
+
+
 // Declare a getFirstTenCountries function and return an array of ten countries. Use different functional programming to work on the countries.js array
 // Declare a getLastTenCountries function which which returns the last ten countries in the countries array.
 // Find out which letter is used many times as initial for a country name from the countries array (eg. Finland, Fiji, France etc)
